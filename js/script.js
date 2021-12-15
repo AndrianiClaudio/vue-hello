617f519b13c71503e69023bb1961496b9bb3e18a
AndrianiClaudio <Claudioandriani1994@gmail.com>
2021-12-15 15:15:50 +0100

    inserimento immagine con src e alt tramite data

--- js/script.js
+++ js/script.js
@@ -6,7 +6,12 @@
 const new_var = new Vue ({
     el: '#main-container',
     data: {
-        text_message: 'Hello Vue' //messagio da stampare
+        text_message: 'Hello Vue', //messagio da stampare
+        image: {
+            src: 'https://i.picsum.photos/id/970/300/300.jpg?hmac=x9ZLs6rRfE1HngAnW7XGcMXjZVep_emsv30sW2gp6k0',
+            // src: '', //test alt
+            alt: "Immagine non trovata!",
+        }
     }
 })
 // console.log(new_var._data.text_message);
