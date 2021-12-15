ed3653390243815117ad006ed8e3d7319d4958d5
AndrianiClaudio <Claudioandriani1994@gmail.com>
2021-12-15 14:53:23 +0100

    stampa messagio in h1 tramite data.

--- js/script.js
+++ js/script.js
@@ -2,9 +2,11 @@
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 // Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 
-// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
-// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
 const new_var = new Vue ({
-    //content
+    el: '#main-container',
+    data: {
+        text_message: 'Hello Vue' //messagio da stampare
+    }
 })
-console.log(new_var);
+// console.log(new_var._data.text_message);
