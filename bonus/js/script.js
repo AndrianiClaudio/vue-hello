85874a3bb640370c57a8fddaf8c0c6431f9f4e03
AndrianiClaudio <Claudioandriani1994@gmail.com>
2021-12-15 16:05:11 +0100

    bonus. acquisizione input tramite target.value

---
+++ bonus/js/script.js
@@ -0,0 +1,22 @@
+// Inseriamo un input per visualizzare in pagina un testo inserito da un utente.
+// Inseriamo una immagine e accanto un numero che identifica il numero di prodotti desiderati, i dati devono venire da Vue.
+// Poi aggiungiamo un pulsante per aumentare questo numero.
+const root = new Vue ({
+    el: '#main-container',
+    data: {
+        input: {
+            type:'text',
+            placeholder: 'inserisci il tuo testo',
+            message: 'Hello vue, this is a message for you.'
+        },
+        image: {
+            src: 'https://i.picsum.photos/id/1037/300/300.jpg?hmac=9F2IJQqPBGhh-aMA1c3bQoLbB45XW7YCMO9LJwDKA2A',
+            alt: 'Immagine non trovata!'
+        }
+    },
+    methods:{
+        showText: function(event) {
+            console.log(event.target.value);
+        }
+    }
+})
--- js/script.js
+++ js/script.js
@@ -1,15 +1,12 @@
 // Descrizione:
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 // Aggiungere alla pagina un’immagine, presa anch’essa da un data.
-
-
 const new_var = new Vue ({
     el: '#main-container',
     data: {
         text_message: 'Hello Vue', //messagio da stampare
         image: {
             src: 'https://i.picsum.photos/id/970/300/300.jpg?hmac=x9ZLs6rRfE1HngAnW7XGcMXjZVep_emsv30sW2gp6k0',
-            // src: '', //test alt
             alt: "Immagine non trovata!",
         }
     }
