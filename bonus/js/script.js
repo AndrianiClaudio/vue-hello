2a6c7416c2b66032a8ea02969b56334ed66c021c
AndrianiClaudio <Claudioandriani1994@gmail.com>
2021-12-15 18:38:48 +0100

    rimosso ._data inutile

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -4,13 +4,11 @@
 const root = new Vue ({
     el: '#main-container',
     data: {
-        // Inseriamo un input per visualizzare in pagina un testo inserito da un utente.
         input: {
             type:'text',
             placeholder: 'inserisci il tuo testo',
             message: 'Hello vue, this is a message for you.'
         },
-        // Inseriamo una immagine e accanto un numero che identifica il numero di prodotti desiderati, i dati devono venire da Vue.
         product: {
             image_src: 'https://i.picsum.photos/id/367/200/200.jpg?hmac=6NmiWxiENMBIeAXEfu9fN20uigiBudgYzqHfz-eXZYk',
             image_alt: 'Immagine non trovata!',
@@ -23,11 +21,11 @@ const root = new Vue ({
     },
     methods:{
         showText: function(event) {
-            this._data.input.message = event.target.value;
+            this.input.message = event.target.value;
         },
         addProduct: function(event) {
             event.preventDefault();
-            this._data.product.product_num += 1;
+            this.product.product_num += 1;
         }
     }
 })
