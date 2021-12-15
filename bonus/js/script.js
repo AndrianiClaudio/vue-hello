028059a92d4bbca95b330d189f1cb50f4741b067
AndrianiClaudio <Claudioandriani1994@gmail.com>
2021-12-15 16:38:17 +0100

    numero prodotti affianco a immagine. aggiunto click

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -4,19 +4,30 @@
 const root = new Vue ({
     el: '#main-container',
     data: {
+        // Inseriamo un input per visualizzare in pagina un testo inserito da un utente.
         input: {
             type:'text',
             placeholder: 'inserisci il tuo testo',
             message: 'Hello vue, this is a message for you.'
         },
-        image: {
-            src: 'https://i.picsum.photos/id/1037/300/300.jpg?hmac=9F2IJQqPBGhh-aMA1c3bQoLbB45XW7YCMO9LJwDKA2A',
-            alt: 'Immagine non trovata!'
+        // Inseriamo una immagine e accanto un numero che identifica il numero di prodotti desiderati, i dati devono venire da Vue.
+        product: {
+            image_src: 'https://i.picsum.photos/id/367/200/200.jpg?hmac=6NmiWxiENMBIeAXEfu9fN20uigiBudgYzqHfz-eXZYk',
+            image_alt: 'Immagine non trovata!',
+            product_num: 10
+        }, 
+        button: {
+            id: 'add-prod',
+            name:'aggiungi prodotto'
         }
     },
     methods:{
         showText: function(event) {
             this._data.input.message = event.target.value;
+        },
+        addProduct: function(event) {
+            event.preventDefault();
+            console.log('click');
         }
     }
 })
