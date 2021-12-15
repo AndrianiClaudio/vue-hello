c777416d1b0cb152a097f4722448a599f4ee3798
AndrianiClaudio <Claudioandriani1994@gmail.com>
2021-12-15 14:39:53 +0100

    creazione repo.

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Descrizione:
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+const new_var = new Vue ({
+    //content
+})
+console.log(new_var);
